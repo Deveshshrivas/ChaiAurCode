2b07d6afa989c4ecc5ae4df02be48772370c0dd8
Dev <deveshshrivas2018@gmail.com>
2024-07-18 02:01:22 +0530

    changes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 app.get('/',(req, res) =>{
     res.send('Server is ready');
 });
-const port = process.env.port || 3000;
+const PORT = process.env.port || 3000;
 
-app.listen(port,()=>{
-    console.log(`Server at http://localhost:${port}`)
+app.listen(PORT,()=>{
+    console.log(`Server at http://localhost:${PORT}`)
 });
